25d79d224ab567e357a3e39ef40903d413ea3605
Manpreet Kunnath <12931966+mvk059@users.noreply.github.com>
2026-02-20 21:34:29 +0530

    Update libraries

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 kotlin {
     androidTarget {
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
+            jvmTarget.set(JvmTarget.JVM_17)
         }
     }
     
@@ -51,6 +51,11 @@ kotlin {
             implementation(libs.compose.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+
+            implementation(libs.kotlinx.dateTime)
+
+            implementation(libs.composables.core)
+            implementation(libs.composables.lucide)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
@@ -80,12 +85,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 
 dependencies {
     debugImplementation(libs.compose.uiTooling)
 }
-
